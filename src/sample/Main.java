91575c48097cec6d6af361a2fd4de1407393cb48
droberts19 <droberts19@scienceleadership.org>
2018-12-20 10:26:21 -0500

    hahaha

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,14 +3,15 @@ package sample;
 public class Main {
 
     public static void main(String[] args) {
-        int numberOf = 10;
+        int numberOf = 9;
 
+        System.out.println();
         System.out.println("Will write " + numberOf + " Fibonacci Strings");
-        System.out.println("");
 
         for (int i = 0; i < numberOf; i++) {
-            Numberz numberz = new Numberz(i+1, 10, 0, 1);
+            Numberz numberz = new Numberz(15, i+0, 1);
             numberz.run();
+            System.out.println();
         }
     }
 
--- src/sample/Numberz.java
+++ src/sample/Numberz.java
@@ -1,19 +1,17 @@
 package sample;
 
 public class Numberz implements Runnable{
-    private int whichOne;
     private int max;
     private int number1;
     private int number2;
 
-    Numberz(int whichString, int maximum, int number1a, int number2a) {
-        whichOne = whichString;
+    Numberz(int maximum, int number1a, int number2a) {
         max = maximum;
         number1 = number1a;
         number2 = number2a;
     }
 
-    private void writeString() {
+    private void writeThing() {
         for (int a = 1; a <= max; ++a) {
             System.out.print(number1 + " ");
             int sum = number1 + number2;
@@ -25,11 +23,9 @@ public class Numberz implements Runnable{
         } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
-        System.out.println();
     }
 
     public void run() {
-        System.out.print("Fibonacci String " + whichOne + " with " + max + " numbers: ");
-        writeString();
+        writeThing();
     }
 }
