987ba77472286110fc62312ba2b48e100eb32cc1
droberts19 <droberts19@scienceleadership.org>
2019-01-03 10:23:07 -0500

    hahaha

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,16 +3,15 @@ package sample;
 public class Main {
 
     public static void main(String[] args) {
-        int numberOf = 9;
+        int numberOf = 5;
 
         System.out.println();
         System.out.println("Will write " + numberOf + " Fibonacci Strings");
 
         for (int i = 0; i < numberOf; i++) {
-            Numberz numberz = new Numberz(15, i+0, 1);
-            numberz.run();
-            //Thread t = new Thread(numberz);
-            //t.start();
+            Numberz numberz = new Numberz(5, i+0, 1);
+            Thread t = new Thread(numberz);
+            t.start();
             System.out.println();
         }
     }
